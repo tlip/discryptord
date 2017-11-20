21eb407db415dc50c834954fb64292ab3b95f23a
Thomas Lipari <thomas.lipari@gmail.com>
2017-11-19 17:16:12 -0800

    add embedded card

--- vendor/lib/types/AxesMap.go
+++ vendor/lib/types/AxesMap.go
@@ -4,12 +4,13 @@ import "time"
 
 // AxesMap :: Map of axes and their metadata
 type AxesMap struct {
-	X      []time.Time
-	Y      []float64
-	Vol    []float64
-	Xmax   float64
-	Ymin   float64
-	Ymax   float64
-	Volmax float64
-	Volmin float64
+	X        []time.Time
+	Y        []float64
+	Vol      []float64
+	VolFixed []float64
+	Xmax     float64
+	Ymin     float64
+	Ymax     float64
+	Volmax   float64
+	Volmin   float64
 }
